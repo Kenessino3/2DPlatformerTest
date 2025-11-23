dfc6dc33fde2c93b78dcc2b659ee0bea90344b79
Kenessino3 <MIJKennedy333@gmail.com>
2025-11-23 03:44:23 +0000

    health

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth {get; private set;}
+    private Animator anim;
+    private bool dead;
+    
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0f, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("Hurt");
+            //iframes
+        }
+        else
+        {
+            if (dead)
+            {
+                anim.SetTrigger("Die");
+                GetComponent.<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    
+}
