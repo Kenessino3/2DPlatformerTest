ec8dd612369c27f2bb7ebf674c4074a875f78794
Kenessino3 <MIJKennedy333@gmail.com>
2025-11-24 14:49:29 +0000

    Added in some traps

---
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class EnemyProjectile : EnemyDamage //damages the player every time they touch
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float resetTime;
+    private float lifetime;
+    
+    public void ActivateProjectile()
+    {
+        lifetime = 0;
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+        
+        lifetime += Time.deltaTime;
+        if (lifetime >= resetTime)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision); //Executes enemy damage first
+        gameObject.SetActive(false); //When this hits any object deactivate arrow
+    }
+}
