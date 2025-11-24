983dd4456967e7916d73e36e642f6bf2de9680cb
Kenessino3 <MIJKennedy333@gmail.com>
2025-11-24 18:19:37 +0000

    Alpha Submission
    added spike head trap and basic melee enemy
    added attack range and box for player
    arranged traps and melee enemy in level for alpha submission

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -35,7 +35,20 @@ public class Health : MonoBehaviour
             if (!dead)
             {
                 anim.SetTrigger("Die");
-                GetComponent<PlayerMovement>().enabled = false;
+                
+                //Player
+                if (GetComponent<PlayerMovement>() != null)
+                {
+                    GetComponent<PlayerMovement>().enabled = false;
+                }
+                
+                
+                //Enemy
+                if (GetComponent<MeleeEnemy>() != null)
+                {
+                    GetComponent<MeleeEnemy>().enabled = false;
+                }
+                
                 dead = true;
             }
         }
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [Header("Attack Settings")]
     [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask enemyLayer;
+    
     private Animator anim;
     private PlayerMovement playerMovement;
     private float cooldownTimer = Mathf.Infinity;
@@ -26,5 +33,33 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetTrigger("Attack");
         cooldownTimer = 0;
+    }
+    
+    private void DamageEnemy()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            boxCollider.bounds.center + transform.right * attackRange * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * attackRange, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0f,
+            Vector2.zero,
+            0f,
+            enemyLayer
+        );
+
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boxCollider == null) return;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(
+            boxCollider.bounds.center + transform.right * attackRange * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * attackRange, boxCollider.bounds.size.y, boxCollider.bounds.size.z)
+        );
     }
 }
